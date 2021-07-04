16e05a10294b96e02b287b363dcc30e705db9474
Amaru333 <amrutesharun0599@gmail.com>
2021-07-04 21:26:31 +0530

    Initial commit

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 const AUTH_URI =
-  "https://accounts.spotify.com/authorize?client_id=7df5f892cd4d4296a2bc5dacf6e27870&response_type=code&redirect_uri=https://gallant-franklin-3c77b7.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=7df5f892cd4d4296a2bc5dacf6e27870&response_type=code&redirect_uri=https://amaru-music-player.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
   return (
