c75bdf37372941a1255caa4c2959ce8b30e4dda5
Amaru333 <amrutesharun0599@gmail.com>
2021-07-04 21:19:55 +0530

    Initial commit

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const AUTH_URI =
+  "https://accounts.spotify.com/authorize?client_id=7df5f892cd4d4296a2bc5dacf6e27870&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URI}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
+}
